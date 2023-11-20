c062c942ef07f8f75df35e876915534c3f3395c9
denisreys <112689124+denisreys@users.noreply.github.com>
2023-11-20 12:01:04 +0400

    created new app

---
+++ resources/js/router.js
@@ -0,0 +1,32 @@
+import { createWebHistory, createRouter } from 'vue-router';
+import Dashboard from './components/Dashboard.vue';
+import Auth from './components/Auth.vue';
+
+const routes = [
+    { 
+        path: '/',
+        name: 'dashboard',
+        component: Dashboard,
+        meta: {requiresAuth: true} 
+    },
+    { 
+        path: '/auth',
+        name: 'auth',
+        component: Auth,
+        meta: {requiresAuth: false} 
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+router.beforeEach((to,from) => {
+    if(to.meta.requiresAuth && !localStorage.getItem('token'))
+        return {name: 'auth'}
+    if(to.meta.requiresAuth == false && localStorage.getItem('token'))
+       return {name: 'dashboard'}   
+});
+
+export default router;
